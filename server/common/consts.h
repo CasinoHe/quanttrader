98f8e6ea26f2bc0d53adb6de82c5c0bd0ee31342
casinohe <casinohe@outlook.com>
2025-01-10 11:19:08 +0800

    update logging configuration: reduce thread count and enhance log pattern format

--- server/common/consts.h
+++ server/common/consts.h
@@ -3,8 +3,8 @@
 namespace quanttrader
 {
     constexpr int kSpdLogQueueSize = 1 << 16;
-    constexpr int kSpdLogThreadsCount = 3;
-    constexpr char kSpdLogPattern[] = "[%Y-%m-%d %H:%M:%S.%e][%z][%n][%^%l%$] %v";
+    constexpr int kSpdLogThreadsCount = 1;
+    constexpr char kSpdLogPattern[] = "[%Y-%m-%d %H:%M:%S.%F%z][%P][%t][%n][%^%l%$] %v";
     constexpr int kSpdLogMaxRotatingFiles = 180;
 
     constexpr char kDefaultLogDir[] = "log";
