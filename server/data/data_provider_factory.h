b8da8f205fee7fa12e64c5c63de1c408dc921543
casinohe <casinohe@outlook.com>
2025-03-22 14:10:59 +0800

    Refactor DataProviderFactory to inherit from Singleton and streamline provider registration methods

--- server/data/data_provider_factory.h
+++ server/data/data_provider_factory.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "data_provider.h"
+#include "common/singleton.h"
 #include <string>
 #include <memory>
 #include <unordered_map>
@@ -10,33 +11,25 @@ namespace quanttrader {
 namespace data {
 namespace provider {
 
+// Function type for creating data providers
+using DataProviderCreatorFunc = std::function<std::shared_ptr<DataProvider>(const std::string_view&, DataParamsType)>;
+
 /**
  * @brief Factory class for creating data providers
  * 
  * This class is responsible for registering and creating different types of data providers.
  * It follows the factory pattern to allow for dynamic provider creation based on type.
  */
-class DataProviderFactory {
-public:
-    /**
-     * @brief Get the singleton instance of the factory
-     * 
-     * @return Reference to the factory instance
-     */
-    static DataProviderFactory& instance() {
-        static DataProviderFactory instance;
-        return instance;
-    }
+class DataProviderFactory : public Singleton<DataProviderFactory> {
 
+public:
     /**
      * @brief Register a data provider type with the factory
      * 
      * @param provider_type The type identifier for the provider
      * @param creator Function that creates a provider of that type
      */
-    void register_provider(
-        const std::string& provider_type,
-        std::function<std::shared_ptr<DataProvider>(const std::string_view&, DataParamsType)> creator) {
+    void register_provider(const std::string& provider_type, DataProviderCreatorFunc creator) {
         creators_[provider_type] = std::move(creator);
     }
 
@@ -60,13 +53,40 @@ public:
         return nullptr;
     }
 
+    /**
+     * Checks if a provider type is registered.
+     * 
+     * @param providerType The type name to check
+     * @return true if the provider type is registered
+     */
+    bool hasProvider(const std::string& providerType) const {
+        return creators_.find(providerType) != creators_.end();
+    }
+
+    /**
+     * Removes a provider type from the registry.
+     * 
+     * @param providerType The type name to remove
+     * @return true if successfully removed, false if not found
+     */
+    bool unregisterProvider(const std::string& providerType) {
+        auto it = creators_.find(providerType);
+        if (it == creators_.end()) {
+            return false;
+        }
+        creators_.erase(it);
+        return true;
+    }
+
 private:
+    friend class Singleton<DataProviderFactory>;
+
     // Private constructor to enforce singleton pattern
     DataProviderFactory() = default;
+    ~DataProviderFactory() = default;
     
     // Map of provider types to creator functions
-    std::unordered_map<std::string, 
-        std::function<std::shared_ptr<DataProvider>(const std::string_view&, DataParamsType)>> creators_;
+    std::unordered_map<std::string, DataProviderCreatorFunc> creators_;
 };
 
 } // namespace provider
